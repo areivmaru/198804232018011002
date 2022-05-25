59b542ca3a149fb3847915ee6f1901401390da6b
Bobby Fajar Christian <kikuluc@gmail.com>
2022-05-25 11:29:02 +0700

    modal kun

--- application/views/cms/pages/v_user.php
+++ application/views/cms/pages/v_user.php
@@ -12,6 +12,7 @@
 
     <!-- Title -->
     <div class="hk-pg-header">
+
         <h4 class="hk-pg-title"><span class="pg-title-icon"><span class="feather-icon"><i data-feather="database"></i></span></span>Master Data Pengguna</h4>
     </div>
     <!-- /Title -->
@@ -20,13 +21,16 @@
     <div class="row">
         <div class="col-xl-12">
             <section class="hk-sec-wrapper">
+                <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModalCenter">
+                    Tambah Pengguna
+                </button>
                 <h5 class="hk-sec-title">Master Data Pengguna</h5>
                 <p class="mb-40">Silahkan gunakan datatables untuk menampilkan data pada table master data Pengguna/</p>
                 <div class="row">
                     <div class="col-sm">
                         <div class="table-wrap">
-                        <table id="tbl_general" class="table table-hover w-100 display">
-                                <thead>  
+                            <table id="tbl_general" class="table table-hover w-100 display">
+                                <thead>
                                 </thead>
                                 <tbody>
                                 </tbody>
@@ -35,8 +39,8 @@
                     </div>
                 </div>
             </section>
-           
-          
+
+
         </div>
     </div>
     <!-- /Row -->
@@ -44,3 +48,23 @@
 </div>
 <!-- /Container -->
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenter" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
